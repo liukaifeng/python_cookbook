0200945cb5a5ab992de7c69e5b7520b9ba6adb25
sunaihua <sunaihua@360.net>
2018-01-18 19:10:06 +0800

    update logic

--- etl/extractors.py
+++ etl/extractors.py
@@ -1,180 +1,209 @@
 # -*- coding:utf-8 -*-
-import hashlib
-import json
 import os
+import json
 import time
+import logging
+import hashlib
 import traceback
-from pprint import pprint
+from elasticsearch import helpers, Elasticsearch, Urllib3HttpConnection
 
-import elasticsearch.helpers
+log = logging.getLogger(__name__)
+FILE_FIELD_TYPE_MAPPING = {
+    'static': ['file_name', 'file_md5', 'file_path', 'file_sha1', 'file_sha256',
+               'file_sha512', 'file_type', 'is_virus', 'threat_level', 'malware_score'],
+}
+FILE_CHECK_RESULT_INDEX_NAME = 'skyeye_cloud_sandbox_file'
+URL_CHECK_RESULT_INDEX_NAME = 'skyeye_cloud_sandbox_url'
 
-import utils
+
+def is_exists(file_dir):
+    return os.path.exists(file_dir)
 
 
-# TODO:需要根据文件提供的方式重写该接口
-def dir_exists(file_dir):
-    return True
-
-
-# TODO:需要根据文件提供的方式重写该接口
 def open_file(file_path):
-    if os.path.exists(file_path):
-        # print file_path
-        with open(file_path, 'r') as data_file:
+    if is_exists(file_path):
+        with open(file_path, 'rb') as data_file:
             data = json.load(data_file)
             return data
     return None
 
 
-def proccess_static_file(file_dir, result_map):
-    static_result = open_file('%s%sstatic.json' % (file_dir, os.path.sep,))
-    if static_result:
-        for field in utils.file_field_type_mapping['static']:
-            result_map[field] = static_result['base_info'].get(field, '')
-        # 需要特殊处理'virus_name'
-        for detail_info in static_result['detail_info'].values():
-            if detail_info.get('virus_name', ''):
-                result_map['virus_name'] = detail_info['virus_name']
-    else:
-        raise Exception("File not exists")
+# 用于读取一个文件包含多行Json的情况
+def open_multi_json_line_file(file_path):
+    if is_exists(file_path):
+        json_list = []
+        with open(file_path, 'rb') as data_file:
+            lines = data_file.readlines()
+            for line in lines:
+                data = json.loads(line)
+                json_list.append(data)
+            return json_list
+    return None
 
 
-def _get_sub_tag_values(dynamic_result, first_tag, second_tag):
-    result_list = []
-    for protocol_actions in dynamic_result[first_tag].values():
-        for protocol_action in protocol_actions:
-            if isinstance(protocol_action, dict) and protocol_action.get(second_tag, ''):
-                result_list.append(protocol_action[second_tag])
-    return list(set(result_list))
+class EsUtils(object):
+    ES_HOSTS = ['1.192.194.26']
 
+    def __init__(self, es_hosts=None):
+        self.es_hosts = es_hosts if es_hosts else self.ES_HOSTS
+        self.es_client = Elasticsearch(hosts=self.es_hosts,
+                                       connection_class=Urllib3HttpConnection,
+                                       sniffer_timeout=60)
 
-def _extract_common_info(dynamic_result, result_map):
-    # signature_rule：signatures/name
-    result_map['signature_rule'] = [signature['name'] for signature in dynamic_result['signatures']]
-    # network_ip:network/*/dip
-    result_map['network_ip'] = _get_sub_tag_values(dynamic_result, 'network', 'dst')
-    # pprint( dynamic_result.get('target', {}).get('file', {}))
-    # network_domain:network/domains/domain
-    result_map['network_domain'] = [domain['domain'] for domain in dynamic_result['network']['domains']]
-    # droped_file:dropped
-    result_map['droped_file'] = [{'name': droped_file['name'], 'type': droped_file['type'], 'md5': droped_file['md5'],
-                                  'sha1': droped_file['sha1'], 'sha256': droped_file['sha256'],
-                                  'sha512': droped_file['sha512'], } for droped_file in
-                                 dynamic_result.get('dropped', [])]
-    # start_time:info/start_time
-    result_map['start_time'] = dynamic_result['info'].get('start_time', '')
-    # end_time:info/end_time
-    result_map['end_time'] = dynamic_result['info'].get('end_time', '')
-    # check_envrionment:info/check_envrionment
-    result_map['check_envrionment'] = dynamic_result['info'].get('check_envrionment', {})
-    # check_status:info/check_envrionment
-    result_map['check_status'] = dynamic_result['info'].get('check_status', {})
+    def proccess_static_file(self, file_dir, result_map):
+        static_result = open_file('%s%sstatic.json' % (file_dir, os.path.sep,))
+        if static_result:
+            for field in FILE_FIELD_TYPE_MAPPING['static']:
+                result_map[field] = static_result['base_info'].get(field, '')
+            # 需要特殊处理'virus_name'
+            for detail_info in static_result['detail_info'].values():
+                if detail_info.get('virus_name', ''):
+                    result_map['virus_name'] = detail_info['virus_name']
+        else:
+            raise Exception("File not exists")
 
+    def _get_sub_tag_values(self, dynamic_result, first_tag, second_tag):
+        result_list = []
+        for protocol_actions in dynamic_result[first_tag].values():
+            for protocol_action in protocol_actions:
+                if isinstance(protocol_action, dict) and protocol_action.get(second_tag, ''):
+                    result_list.append(protocol_action[second_tag])
+        return list(set(result_list))
 
-def proccess_file_runtime_file(file_dir, result_map):
-    dynamic_result = open_file('%s%sreports/report.json' % (file_dir, os.path.sep,))
-    # print dynamic_result
-    if dynamic_result:
-        # file_score:info/score
-        result_map['file_score'] = dynamic_result['info']['score']
-        # signature_info:static/signature
-        result_map['signature_info'] = dynamic_result.get('static', {}).get('signature', [])
-        # network_url:target/urls
-        result_map['network_url'] = dynamic_result.get('target', {}).get('file', {}).get('urls', [])
-        _extract_common_info(dynamic_result, result_map)
-    else:
-        raise Exception("File not exists")
+    def _extract_common_info(self, dynamic_result, result_map):
+        # signature_rule：signatures/name
+        result_map['signature_rule'] = [signature['name'] for signature in dynamic_result['signatures']]
+        # network_ip:network/*/dip
+        result_map['network_ip'] = self._get_sub_tag_values(dynamic_result, 'network', 'dst')
+        result_map['network_domain'] = [domain['domain'] for domain in dynamic_result['network']['domains']]
+        # droped_file:dropped
+        result_map['droped_file'] = [{'name': droped_file['name'], 'type': droped_file['type'],
+                                      'md5': droped_file['md5'], 'sha1': droped_file['sha1'],
+                                      'sha256': droped_file['sha256'], 'sha512': droped_file['sha512']}
+                                     for droped_file in dynamic_result.get('dropped', [])]
+        # start_time:info/start_time
+        # result_map['start_time'] = dynamic_result['info'].get('start_time', '')
+        # end_time:info/end_time
+        # result_map['end_time'] = dynamic_result['info'].get('end_time', '')
 
+    def proccess_file_runtime_file(self, file_dir, result_map):
+        dynamic_result = open_file('%s%sreports/report.json' % (file_dir, os.path.sep,))
+        if dynamic_result:
+            # file_score:info/score
+            result_map['file_score'] = dynamic_result['info']['score']
+            # signature_info:static/signature
+            result_map['signature_info'] = dynamic_result.get('static', {}).get('signature', [])
+            # network_url:target/urls
+            result_map['network_url'] = dynamic_result.get('target', {}).get('file', {}).get('urls', [])
+            self._extract_common_info(dynamic_result, result_map)
+        else:
+            raise Exception("File not exists")
 
-def proccess_url_runtime_file(file_dir, result_map):
-    dynamic_result = open_file('%s%sreports/report.json' % (file_dir, os.path.sep,))
-    # print dynamic_result
-    if dynamic_result:
-        target_url = dynamic_result.get('target', {}).get('url', '')
-        result_map['url'] = target_url
-        result_map['url_md5'] = hashlib.md5(target_url).hexdigest()
-        result_map['url_sha1'] = hashlib.sha1(target_url).hexdigest()
-        result_map['url_score'] = dynamic_result['info']['score']
-        # network_url:target/urls
-        url_list = []
-        for dropped_action in dynamic_result['dropped']:
-            for dropped_url in dropped_action.get('urls', []):
-                url_list.append(dropped_url)
-        result_map['network_url'] = list(set(url_list))
-        _extract_common_info(dynamic_result, result_map)
-    else:
-        raise Exception("File not exists")
+    def proccess_task_file(self, file_dir, result_map):
+        task_info = open_file('%s%s/task.json' % (file_dir, os.path.sep,))
+        if task_info:
+            print task_info['submit_info']
+            result_map['task_id'] = task_info['submit_info']['task_id']
+            result_map['start_time'] = task_info['submit_info']['accept_time']
+            result_map['end_time'] = task_info['submit_info']['finish_time']
+            result_map['sample_path'] = task_info['submit_info']['remote_path']
+            result_map['result_path'] = task_info['submit_info']['result_path']
+            result_map['check_status'] = task_info['submit_info'].get('check_status', {})
+            result_map['check_envrionment'] = {'machine': task_info['submit_info']['check_param']['machine'],
+                                               'analyzer': task_info['submit_info']['analyze_type']}
 
+        else:
+            raise Exception("File not exists")
 
-def write_result_to_es(result_map, index, doc_type='result'):
-    try:
-        response = utils.es_client.index(index=index, doc_type=doc_type, body=result_map)
-        if response.get('created', '') != True:
-            traceback.print_exc()
-            # raise Exception('write es failed')
-    except BaseException, ex:
-        print ex
-        traceback.print_exc()
+    def proccess_shot_file(self, file_dir, result_map):
+        jsons = open_multi_json_line_file('%s%s/files.json' % (file_dir, os.path.sep,))
+        if jsons:
+            screen_list = []
+            for json_line in jsons:
+                if json_line.get('path','').startswith('shots'):
+                    screen_list.append(json_line['path'])
+            result_map['screen'] = list(set(screen_list))
+        else:
+            raise Exception("File not exists")
 
+    def proccess_url_runtime_file(self, file_dir, result_map):
+        dynamic_result = open_file('%s%sreports/report.json' % (file_dir, os.path.sep,))
+        if dynamic_result:
+            target_url = dynamic_result.get('target', {}).get('url', '')
+            result_map['url'] = target_url
+            result_map['url_md5'] = hashlib.md5(target_url).hexdigest()
+            result_map['url_sha1'] = hashlib.sha1(target_url).hexdigest()
+            result_map['url_score'] = dynamic_result['info']['score']
+            url_list = []
+            for dropped_action in dynamic_result['dropped']:
+                for dropped_url in dropped_action.get('urls', []):
+                    url_list.append(dropped_url)
+            result_map['network_url'] = list(set(url_list))
+            self._extract_common_info(dynamic_result, result_map)
+        else:
+            raise Exception("File not exists")
 
-def write_result_to_es_by_bulk(result_maps, index, doc_type='result'):
-    try:
-        actions = [
-            {
-                '_op_type': 'index',
-                '_index': index,
-                '_type': doc_type,
-                '_source': d
-            }
-            for d in result_maps
-        ]
-        response = elasticsearch.helpers.bulk(client=utils.es_client, actions=actions)
-        print response
-    except BaseException, ex:
-        print ex
-        traceback.print_exc()
+    def write_result_to_es(self, result_map, index, doc_type='result'):
+        response = self.es_client.index(index=index, doc_type=doc_type, body=result_map)
+        if not response.get('created', ''):
+            raise Exception('write es failed')
 
-
-def extractor_file_check_result(file_dir):
-    if dir_exists(file_dir):
+    def write_result_to_es_by_bulk(self, result_maps, index, doc_type='result'):
         try:
-            result_map = {}
-            s_time = time.time()
-            proccess_static_file(file_dir, result_map)
-            proccess_file_runtime_file(file_dir, result_map)
-            # 处理result_path
-            result_map['result_path'] = file_dir
-            pprint(result_map)
-            write_result_to_es(result_map, utils.file_check_result_index_name)
-            print 'Process %s dir elapsed:%s ' % (file_dir, str(time.time() - s_time))
-            return True
-        except Exception, ex:
-            print '####:', ex
+            actions = [
+                {
+                    '_op_type': 'index',
+                    '_index': index,
+                    '_type': doc_type,
+                    '_source': d
+                }
+                for d in result_maps
+            ]
+            response = helpers.bulk(client=self.es_client, actions=actions)
+            log.info('Response: %s', response)
+        except BaseException, ex:
+            log.error('Error: %s', ex)
             traceback.print_exc()
-            return False
-    return False
 
+    def extractor_file_check_result(self, file_dir):
+        if is_exists(file_dir):
+            try:
+                result_map = {}
+                s_time = time.time()
+                self.proccess_static_file(file_dir, result_map)
+                self.proccess_file_runtime_file(file_dir, result_map)
+                self.proccess_task_file(file_dir, result_map)
+                self.proccess_shot_file(file_dir, result_map)
+                self.write_result_to_es(result_map, FILE_CHECK_RESULT_INDEX_NAME)
+                log.info('Process %s dir elapsed:%s ', file_dir, str(time.time() - s_time))
+                return True
+            except Exception, ex:
+                log.error('Error: %s', ex)
+                traceback.print_exc()
+                return False
+        return False
 
-def extractor_url_check_result(file_dir):
-    if dir_exists(file_dir):
-        try:
-            result_map = {}
-            s_time = time.time()
-            proccess_url_runtime_file(file_dir, result_map)
-            # 处理result_path
-            result_map['result_path'] = file_dir
-            write_result_to_es(result_map, utils.url_check_result_index_name)
-            print 'Process %s dir elapsed:%s ' % (file_dir, str(time.time() - s_time))
-            pprint(result_map)
-            return True
-        except Exception, ex:
-            print '####:', ex
-            traceback.print_exc()
-            return False
-    return False
+    def extractor_url_check_result(self, file_dir):
+        if is_exists(file_dir):
+            try:
+                result_map = {}
+                s_time = time.time()
+                self.proccess_url_runtime_file(file_dir, result_map)
+                self.proccess_task_file(file_dir, result_map)
+                self.proccess_shot_file(file_dir, result_map)
+                self.write_result_to_es(result_map, URL_CHECK_RESULT_INDEX_NAME)
+                log.info('Process %s dir elapsed:%s ', file_dir, str(time.time() - s_time))
+                return True
+            except Exception, ex:
+                log.error('Error: %s', ex)
+                traceback.print_exc()
+                return False
+        return False
 
 
 if __name__ == '__main__':
-    # extractor_file_check_result("D:/sc/git/python_cookbook/etl/4458_ce52d039f3229e6ba37247bbe0eee31e")
-    # extractor_url_check_result("D:/sc/git/python_cookbook/etl/0336_ab877511ce28325f31f1e398827834fa")
-    extractor_url_check_result("D:/sc/git/python_cookbook/etl/2901_ab877511ce28325f31f1e398827834fa")
+    es = EsUtils()
+    # es.extractor_file_check_result("D:/sc/git/python_cookbook/etl/4458_ce52d039f3229e6ba37247bbe0eee31e")
+    # es.extractor_url_check_result("D:/sc/git/python_cookbook/etl/0336_ab877511ce28325f31f1e398827834fa")
+    # es.extractor_url_check_result("D:/sc/git/python_cookbook/etl/2901_ab877511ce28325f31f1e398827834fa")
+    es.extractor_url_check_result("D:/sc/git/python_cookbook/etl/645302318a754322c5d3209f8d444f5bf0b051ee")
